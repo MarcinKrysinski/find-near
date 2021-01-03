7d8c0b3a2174c5310ef9b29c77bf5b77d19967c7
Marcin <marcin.jakubkrysinski@gmail.com>
2021-01-03 23:30:16 +0100

    Refactoring and fix URL

--- src/main/java/pl/krysinski/findnear/ApiClient.java
+++ src/main/java/pl/krysinski/findnear/ApiClient.java
@@ -1,30 +1,44 @@
 package pl.krysinski.findnear;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
-import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import pl.krysinski.findnear.model.PlaceDTO;
 import pl.krysinski.findnear.model.api.PlaceUrl;
 import pl.krysinski.findnear.model.api.Result;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Component
 public class ApiClient {
 
+    @Value("${api-key}")
+    private String apiKey;
+    private String query = "kebab";
+    private String lat = "54.3507477,";
+    private String lng = "18.6495267";
+    private String separator = ",";
+    private String rad = "10000";
+    private static final String GOOGLE_MAPS_BASE_API_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
 
     @EventListener(ApplicationReadyEvent.class)
     public void get() {
         List<PlaceDTO> placeDtoList = new ArrayList<>();
         RestTemplate restTemplate = new RestTemplate();
-        PlaceUrl object = restTemplate.getForObject("https://maps.googleapis.com/maps/api/place/textsearch/json?query=kebab&location=54.385327,18.606870&radius=10000&key=AIzaSyCPl_X8_l5ZBIQYLKCotZUV2uw4T6K93Xk", PlaceUrl.class);
+        PlaceUrl object = restTemplate.getForObject(encodeUrl(), PlaceUrl.class);
         object.getResults().forEach(o -> placeDtoList.add(modelMapper().map(o, PlaceDTO.class)));
         System.out.println("Lista: " + placeDtoList);
     }
@@ -49,4 +63,31 @@ public class ApiClient {
 
         return modelMapper;
     }
+
+    private URI getApiUrl() {
+        URI uri = null;
+        try {
+            URIBuilder uriBuilder = new URIBuilder(GOOGLE_MAPS_BASE_API_URL);
+            uriBuilder.addParameter("query", query);
+            uriBuilder.addParameter("location", lat + separator + lng);
+            uriBuilder.addParameter("radius", rad);
+            uriBuilder.addParameter("key", apiKey);
+            uri = uriBuilder.build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return uri;
+    }
+
+    private String encodeValue(String value) throws UnsupportedEncodingException {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+    }
+
+    private String encodeUrl() {
+        String str = String.valueOf(getApiUrl());
+        String encodeStr = str.replace("%2C%2C",",");
+        return encodeStr;
+    }
+
+
 }
