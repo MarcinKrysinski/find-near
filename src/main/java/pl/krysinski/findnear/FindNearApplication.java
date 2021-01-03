3eb4387a48f1f9e5e711ade999eedc0fde168876
Marcin <marcin.jakubkrysinski@gmail.com>
2021-01-03 17:54:26 +0100

    Initial commit

---
+++ src/main/java/pl/krysinski/findnear/FindNearApplication.java
@@ -0,0 +1,13 @@
+package pl.krysinski.findnear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindNearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindNearApplication.class, args);
+    }
+
+}
