d748257f955aa5cbdd60e1f831c70faf3dfa3b5f
Marcin <marcin.jakubkrysinski@gmail.com>
2021-01-04 20:28:58 +0100

    added amp and current position

---
+++ src/main/resources/static/map.js
@@ -0,0 +1,43 @@
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 54.35205, lng: 18.64637 },
+        zoom: 11,
+    });
+
+    let infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here!');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
+
+
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
---
+++ src/test/java/pl/krysinski/findnear/FindNearApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krysinski.findnear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindNearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
